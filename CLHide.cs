0e0d2413d9b8ccd14681880f889820331f2e5a90
heezy lee <zerse@papayaloop.com>
2017-12-17 16:16:55 +0900

    add CLHide, CLShow

---
+++ CLHide.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace CocosLikeActions {
+  public class CLHide : CLInstantAction {
+    public override CLAction Clone() {
+      return new CLHide();
+    }
+
+    public override CLAction Reverse() {
+      return new CLShow();
+    }
+
+    protected override void Update( float progress ) {
+      SetRendererDisabled( Target, false );
+    }
+
+    private static void SetRendererDisabled( GameObject gameObject, bool applyToChildren ) {
+      var renderer = gameObject.GetComponent<Renderer>();
+      if ( renderer != null && renderer.enabled ) {
+        renderer.enabled = false;
+      }
+
+      if ( applyToChildren ) {
+        var renderers = gameObject.GetComponentsInChildren<Renderer>();
+        foreach ( var childRenderer in renderers ) {
+          childRenderer.enabled = false;
+        }
+      }
+    }
+  }
+}
---
+++ CLShow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace CocosLikeActions {
+  public class CLShow : CLInstantAction {
+    public override CLAction Clone() {
+      return new CLShow();
+    }
+
+    public override CLAction Reverse() {
+      return new CLHide();
+    }
+
+    protected override void Update( float progress ) {
+      SetRendererEnabled( Target, false );
+    }
+
+    private static void SetRendererEnabled( GameObject gameObject, bool applyToChildren ) {
+      var renderer = gameObject.GetComponent<Renderer>();
+      if ( renderer != null && ! renderer.enabled ) {
+        renderer.enabled = true;
+      }
+
+      if ( applyToChildren ) {
+        var renderers = gameObject.GetComponentsInChildren<Renderer>();
+        foreach ( var childRenderer in renderers ) {
+          childRenderer.enabled = true;
+        }
+      }
+    }
+  }
+}
